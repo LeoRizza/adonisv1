70481b0edd608e5d8e7ff5b1a6ad65220e6a8a9a
Leo <leonardorizza@hotmail.com>
2025-06-22 17:42:48 -0300

    add basic product model and CRUD

--- start/routes.ts
+++ start/routes.ts
@@ -3,6 +3,7 @@ import { middleware } from '#start/kernel'
 /* import userController from '../app/controllers/users_controller.js' */
 const UsersController = () => import('../app/controllers/users_controller.js')
 const AuthController = () => import('../app/controllers/auth_controller.js')
+const ProductsController = () => import('../app/controllers/products_controller.js')
 
 router.get('/', async () => {
   return {
@@ -21,6 +22,16 @@ router
         router.get('user/:id', [UsersController, 'show'])
         router.delete('user/:id', [UsersController, 'destroy'])
         router.put('user/:id', [UsersController, 'update'])
+      })
+      .middleware([middleware.jwt(), middleware.role({ roles: ['admin', 'god'] })])
+
+    router
+      .group(() => {
+        router.get('products', [ProductsController, 'index'])
+        router.post('products', [ProductsController, 'store'])
+        router.get('products/:id', [ProductsController, 'show'])
+        router.put('products/:id', [ProductsController, 'update'])
+        router.delete('products/:id', [ProductsController, 'destroy'])
       })
       .middleware([middleware.jwt(), middleware.role({ roles: ['admin', 'god'] })])
   })
